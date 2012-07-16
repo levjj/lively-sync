2b3aef193a12ae85884f24bf20523523eba29037
Christopher Schuster <cs@livoris.de>
2012-07-16 21:30:51 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/client.js

--- client.js
+++ client.js
@@ -1,50 +1,99 @@
 /**
+
  * Client part of syncing
+
  * (morphic integration, etc.)
+
  */
+
+
 
 module('users.cschuster.sync.client').requires('users.cschuster.sync.shared').toRun(function() {
 
+
+
 users.cschuster.sync.diff.Snapshot.addMethods({
-     getSerializer: function() {
+
+     getSerializer: function(data) {
+
         var serializer = ObjectGraphLinearizer.forNewLivelyCopy();
+
         serializer.addPlugins(lively.persistence.getPluginsForLively());
+
         var p = new GenericFilter();
+
         p.addFilter(function(obj, prop, value) {
+
             return obj.isScript && prop == "currentTimeout";
+
         });
+
         serializer.addPlugins([p]);
+
         serializer.showLog = false;
+
         return serializer;
+
     },
+
     createFromMorphs: function(morphs) {
+
     
+
     }
+
 });
+
+
 
 users.cschuster.sync.diff.Patch.addMethods({
+
     applyToMorphs: function(morphs) {
+
     
+
     }
+
 });
+
+
 
 cop.create("SyncNewMorphs").refineObject(this.world(), {
+
     addMorph: function(morph, optMorphBefore) {
+
         that.morphSyncTable[morph.id] = morph;
+
         return cop.proceed(morph, optMorphBefore);
+
     },
+
     removeMorph: function(morph) {
+
         delete that.morphSyncTable[morph.id]
+
         return cop.proceed(morph);
+
     }
+
 });
 
+
+
 cop.create("HierachicalIds").refineClass(lively.persistence.ObjectGraphLinearizer, {
+
     newId: function() {
+
         var id = this.path.join('/');
+
         while (this.registry[id]) id = "#" + id;
+
         return id;
+
     }
+
 }).beGlobal();
+
+
 
 });
