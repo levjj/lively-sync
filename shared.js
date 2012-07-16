3bb4f068f6e0ff044bc44d4d1b832ce03ab9ba88
Christopher Schuster <cs@livoris.de>
2012-07-16 23:21:54 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/shared.js

--- shared.js
+++ shared.js
@@ -48,8 +48,45 @@ module('users.cschuster.sync.shared').requires('users.cschuster.sync.jsondiffpat
             });
             return toDelete;
         },
+        convertToForwardPatch: function(obj, optObj) {
+            if (optObj) obj = optObj;
+            // discards data for reverse patching
+            if (Object.isObject(obj)) {
+                if (Array.isArray(obj)) { // instruction
+                    if (obj.length !== 1) {
+                        obj.splice(0, 1);
+                    }
+                } else { // raw object or array
+                    delete obj._t;
+                    Properties.forEachOwn(obj, this.convertToForwardPatch, this)
+                }
+            }
+        },
+        removeSmartRefs: function(obj, rawMode) {
+            // discards SmartRefs
+            if (Object.isObject(obj)) {
+                if (obj.__isSmartRef__) return true;
+                if (!rawMode && Array.isArray(obj)) { // instruction
+                    if (obj.length === 1) {
+                        if (this.removeSmartRefs(obj[0], true)) {
+                            return true;
+                        }
+                    }
+                } else { // raw object or array
+                    Properties.forEachOwn(obj, function(key, value) {
+                        if (Object.isObject(value) &&
+                            value.__isSmartRef__ ||
+                            this.removeSmartRefs(value, rawMode)) {
+                            delete obj[key];
+                        }
+                    }, this)
+                    if (Object.isEmpty(obj)) return true;
+                }
+            }
+            return false;
+        },
         toPatch: function() {
-            var patch = new users.cschsuter.sync.Patch();
+            var patch = new users.cschuster.sync.Patch();
             var toDelete = this.aggregateDeletions();
             for (var id in this.data.registry) {
                 if (id && !id.startsWith("#")
