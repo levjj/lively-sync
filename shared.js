a5aec382fa0374f3ab0cc349cd6eff0c2714243a
Christopher Schuster <cs@livoris.de>
2012-07-16 23:15:17 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/shared.js

--- shared.js
+++ shared.js
@@ -35,9 +35,32 @@ module('users.cschuster.sync.shared').requires('users.cschuster.sync.jsondiffpat
         apply: function(snapshot) {
             jsondiffpatch.patch(snapshot.data, this.data);
         },
+        aggregateDeletions: function() {
+            var toDelete = [];
+            Properties.forEachOwn(this.data.registry, function(key, value) {
+                if (Array.isArray(value) && value.length == 3) {
+                    for (var i = 0; i < toDelete.length; i++) {
+                        if (toDelete[i].startsWith(key))
+                            toDelete.removeAt(i--);
+                        toDelete.push(key + "/");
+                    }
+                }
+            });
+            return toDelete;
+        },
         toPatch: function() {
             var patch = new users.cschsuter.sync.Patch();
-            var redundantDeletes = this.findRedundandDeletes();
+            var toDelete = this.aggregateDeletions();
+            for (var id in this.data.registry) {
+                if (id && !id.startsWith("#")
+                       && !toDelete.any(function(s) {return id.startsWith(s)})) {
+                    var obj = this.data.registry[id];
+                    this.convertToForwardPatch(obj);
+                    if (!this.removeSmartRefs(obj)) {
+                        patch.data[id] = obj;
+                    }
+                }
+            }
             return patch;
         },
         toJSON: function() {
