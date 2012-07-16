699119de0db95277f525f6352ba80ee5fd0acdfd
Christopher Schuster <cs@livoris.de>
2012-07-16 21:55:53 +0000

    removed parsing bug

--- tests.js
+++ tests.js
@@ -22,20 +22,3 @@ TestCase.subclass('users.cschuster.sync.tests.DiffTest',
     }
 });
 }) // end of module
-
-
-odule('users.cschuster.sync.tests').requires('lively.TestFramework').toRun(function() {
-
-TestCase.subclass('users.cschuster.sync.tests.DiffTest',
-'helper', {
-    setUp: function() {
-        this.tracker = new users.cschuster.Tests.LifeTracker();
-    }
-},
-'testing', {
-    testInitial: function() {
-        this.assertEquals(this.tracker.getLife(), 20);
-        this.assert(this.tracker.isAlive());
-        this.assert(!this.tracker.isDead());
-    },
-}) // end of module
