60f68335d4ec3e02a7e1d0381a166574a8efd1d5
Christopher Schuster <cs@livoris.de>
2012-07-24 19:03:42 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -178,6 +178,9 @@ lively.morphic.tests.TestCase.subclass('users.cschuster.sync.tests.MorphPatchTes
     }
 },
 'assertion', {
+    assertMorphNode: function(expected) {
+        this.assertNodeMatches(expected, this.morph.renderContext().getMorphNode());   
+    },
     assertShapeNode: function(expected) {
         this.assertNodeMatches(expected, this.morph.renderContext().getShapeNode());   
     }
