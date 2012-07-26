b341b80242ba79739bcc7de8497ed1c7a2fd7f8f
Christopher Schuster <cs@livoris.de>
2012-07-26 21:57:20 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -137,7 +137,6 @@ TestCase.subclass('users.cschuster.sync.tests.DiffTest',
         var snapshotB = this.serialize(this.table);
         var expected = {};
         expected[this.rect.id + "/submorphs/0"] = [0,0];
-        debugger;
         this.assertPatch(expected, snapshotA, snapshotB);
     },
     testDiffingDoesNotAffectSnapshot: function() {
