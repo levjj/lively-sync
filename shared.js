b4f6eb3ffe7e8bcc81d0d23d670d456f8192fc81
Christopher Schuster <cs@livoris.de>
2012-07-27 22:14:35 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/shared.js

--- shared.js
+++ shared.js
@@ -89,8 +89,8 @@ Object.subclass('users.cschuster.sync.Diff', {
                 delete obj[key];
             }
         }, this);
-        if (typeof obj.__LivelyClassName__ == "undefined")
-            delete obj.__LivelyClassName;
+        if (obj.__LivelyClassName__ === undefined)
+            delete obj.__LivelyClassName__;
         // always keep empty objects and arrays in raw mode
         return false;
     },
