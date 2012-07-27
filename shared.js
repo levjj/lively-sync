178cd3f672b9db744f955dc31b0095867702b1e5
Christopher Schuster <cs@livoris.de>
2012-07-27 22:19:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/shared.js

--- shared.js
+++ shared.js
@@ -181,8 +181,10 @@ Object.subclass('users.cschuster.sync.Patch', {
     addMissingClassNames: function(obj) {
         if (typeof obj == "object") {
             if (Array.isArray(obj)) { // instruction
-                if (obj.length < 3 && !obj.last().hasOwnProperty("__LivelyClassName__")) {
-                    obj.last().__LivelyClassName__ = undefined;
+                if (obj.length == 3) return;
+                var o = obj.last();
+                if (o && typeof o == "object" && !o.hasOwnProperty("__LivelyClassName__")) {
+                    o.__LivelyClassName__ = undefined;
                 }
             } else { // raw object or array
                 Properties.forEachOwn(obj, function(name, val) {
