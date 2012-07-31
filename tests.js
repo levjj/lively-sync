07b06fe0a0dc3476ca1c4da30da853861090178f
Christopher Schuster <cs@livoris.de>
2012-07-31 22:43:55 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -490,6 +490,9 @@ lively.morphic.tests.TestCase.subclass('users.cschuster.sync.tests.MorphPatchTes
                 )
             )
         );
+    },
+    testOpenObjectInspector: function() {
+        
     }
 });
 }) // end of module
