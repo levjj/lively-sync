ed67f4799650c862555ad36bcc25b520fd1ea9ae
Christopher Schuster <cs@livoris.de>
2012-08-06 23:38:33 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -328,11 +328,12 @@ lively.morphic.tests.MorphTests.subclass('users.cschuster.sync.tests.DiffTest',
         var snapshotB = this.serialize(this.table);
         var expected = {};
         expected[this.rect.id] = {attributeConnections:[[]],
-                                      doNotCopyProperties:[["$$a"]],doNotSerialize:[["$$a"]]};
+                                  doNotCopyProperties:[["$$a"]],doNotSerialize:[["$$a"]]};
         expected[this.rect.id + "/attributeConnections/0"] = [{
             sourceAttrName:"a",sourceObj:{__isSmartRef__:true,id:this.rect.id},
             targetMethodName:"b",targetObj:{__isSmartRef__:true,id:this.rect.id},
-            __LivelyClassName__:"AttributeConnection",__SourceModuleName__:"Global.lively.bindings"}];
+            __LivelyClassName__:"AttributeConnection",
+            __SourceModuleName__:"Global.lively.bindings"}];
         this.assertPatch(expected, snapshotA, snapshotB);
     }
 });
