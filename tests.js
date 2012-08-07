7cffc189fbc679107a7224501c1ea1a27e580a1b
Christopher Schuster <cs@livoris.de>
2012-08-07 02:24:43 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -816,6 +816,7 @@ lively.morphic.tests.MorphTests.subclass('users.cschuster.sync.tests.SyncWorldsT
         var h = this.worldA.firstHand();
         h.grabMorph(box);
         this.wcA.removeObject(box);
+        debugger;
         this.sync();
         this.assertSync(3);
         h.setPosition(pt(400,100));
