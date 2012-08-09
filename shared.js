1541ab60b83a7b4767d7927ebbdadbf622ba3424
Christopher Schuster <cs@livoris.de>
2012-08-09 06:23:05 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/shared.js

--- shared.js
+++ shared.js
@@ -293,6 +293,7 @@ Object.subclass('users.cschuster.sync.Patch', {
         return diff;
     },
     apply: function(snapshot) {
+        this.recreate(snapshot);
         var diff = this.toDiff(snapshot);
         diff.apply(snapshot);
     },
