e5b8bfe8955a5bc05ef918d0f3a1c331da69c0d9
Christopher Schuster <cs@livoris.de>
2012-08-13 18:17:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -311,7 +311,6 @@ lively.morphic.tests.MorphTests.subclass('users.cschuster.sync.tests.DiffTest',
         var snapshotE = this.serialize({X:x});
         var expected = {"X/a/0":[{id: "Y", name: "y"}],
                         "X/a/1": ["X/a/0", {}, 0]};
-        debugger;
         this.assertPatch(expected, snapshotD, snapshotE);
         x.a.removeAt(0);
         var snapshotF = this.serialize({X:x});
