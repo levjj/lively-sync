c0a3b430444a85a38a837be3e667d4cec8db1d1a
Christopher Schuster <cs@livoris.de>
2012-08-13 22:33:50 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/shared.js

--- shared.js
+++ shared.js
@@ -563,32 +563,7 @@ Object.subclass('users.cschuster.sync.Patch', {
         if (typeof json == 'string') json = JSON.parse(json);
         this.data = json || {};
     },
-    convertToDiffInstruction: function(obj, optSnapshotObj, optSnapshot) {
-        // recreates diff instructions from patch
-        if (typeof obj == "object") {
-            if (Array.isArray(obj)) { // instruction
-                if (obj.length == 2) { // delete
-                    obj.unshift(optSnapshotObj !== undefined ? optSnapshotObj : 0);
-                } else if (obj.length == 3) { // move
-                    obj.unshift(0);
-                    this.convertToDiffInstruction(obj[2],
-                        optSnapshot && optSnapshot.registry[obj[1]], optSnapshot);
-                } else if (optSnapshotObj !== undefined) { // add or set
-                    obj.unshift(optSnapshotObj);
-                }
-            } else { // path object or array
-                // recursive call
-                Properties.forEachOwn(obj, function(name, val) {
-                    this.convertToDiffInstruction(val, optSnapshotObj[name], optSnapshot);
-                }, this);
-                // adding _t back if this is an array
-                var isntArray = Properties.own(obj).find(function(name) {
-                    return isNaN(name);
-                });
-                if (!Object.isEmpty(obj) && !isntArray) obj._t = "a";
-            }
-        }
-    },
+
     toDiff: function() {
         var raw = {registry:{}};
         for (var key in this.data) {
