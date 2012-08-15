aafa0fec9ab26591b97de62e174d8481fa64d7e6
Christopher Schuster <cs@livoris.de>
2012-08-15 00:43:21 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/client.js

--- client.js
+++ client.js
@@ -27,7 +27,7 @@ users.cschuster.sync.Plugin.subclass('users.cschuster.sync.MorphPlugin',
                 obj.owner = oldOwner; // neccessary for proper removing in addMorph
             }
         }
-        //var firstHand = this.world.submorphs.find(function(m) { return m.isHand });
+        var firstHand = this.world.submorphs.find(function(m) { return m.isHand });
         var firstHand;
         this.world.addMorph(obj, firstHand);
     }
