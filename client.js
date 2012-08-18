8262d466150def007e4d6ac45c657aaca7c0faed
Christopher Schuster <cs@livoris.de>
2012-08-18 00:27:54 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/client.js

--- client.js
+++ client.js
@@ -448,7 +448,8 @@ Object.subclass('users.cschuster.sync.WorkingCopy',
             arraysToRepair.invoke('repair');
             // apply all 'additions' at once
             for (var i = 0; i < moves.length; i++) {
-                this.set(moves[i].to.obj, moves[i].to.prop, moves[i].from.obj);
+                if (moves[i].to)
+                    this.set(moves[i].to.obj, moves[i].to.prop, moves[i].from.obj);
             }
         }
     },
