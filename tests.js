01c2da01bb201a49a2df86ff4e367f99e44cd869
Christopher Schuster <cs@livoris.de>
2012-08-20 21:10:23 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -1519,7 +1519,7 @@ users.cschuster.sync.tests.SyncTest.subclass('users.cschuster.sync.tests.Interac
 'running', {
     setUp: function($super) {
         $super();
-        this.morph = this.newBox(20, 20, "X");
+        this.morph = this.newBox(20, 20, "X", Color.web.yellow);
         this.openInWorldA(this.morph);
         this.sync();
         this.rev = 2;
