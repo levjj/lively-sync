0e60eb088e93af96eb77cd8d5d7585cc256a27c6
Christopher Schuster <cs@livoris.de>
2012-08-27 19:10:14 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -1524,7 +1524,7 @@ users.cschuster.sync.tests.SyncTest.subclass('users.cschuster.sync.tests.SyncPri
     testWindow: function() {
         var box = this.addBox("X");
         box.setExtent(pt(80,40));
-        this.worldA.internalAddWindow(box, box.name, pt(40, 20));
+        this.worldA.internalAddWindow(box, box.name, pt(10, 10));
         this.wcA.removeObject(box);
         this.wcA.addObject(box.owner);
         /* The label of the window title bar is broken
