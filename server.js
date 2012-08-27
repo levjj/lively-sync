f2041452b99a2b8dd9353fb0ec9b72d2589cba84
Christopher Schuster <cs@livoris.de>
2012-08-27 21:43:47 +0000

    debugging stack overflow

--- server.js
+++ server.js
@@ -76,9 +76,12 @@ Object.subclass('users.cschuster.sync.Repository', {
         }.bind(this));
     },
     
+    counter: 0,
+    
     checkout: function(rev, cb) {
         this.latestSnapshotRevBefore(rev, function(from) {
             this.db.query("SELECT rev, type, data FROM history WHERE obj = $1 AND rev >= $2 AND rev <= $3 ORDER BY rev", [this.channel, from, rev], function(err, result) {
+                if (this.counter++ > 100) return this.handleError("stack overflow");
                 try {
                     if (err) return this.handleError(err);
                     if (result.rows.length < 1) return this.handleError("checkout: no revision between " + from + " and " + rev);
