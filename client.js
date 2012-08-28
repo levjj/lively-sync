874766f4998976817065b0138bbf7b770d066839
Christopher Schuster <cs@livoris.de>
2012-08-28 18:43:17 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/client.js

--- client.js
+++ client.js
@@ -126,14 +126,15 @@ users.cschuster.sync.Plugin.subclass('users.cschuster.sync.MorphPlugin',
                         var morph = value.shift()
                         if (morph !== this.world.firstHand()) morph.remove();
                     } else { // add, set or move
+                        var length = parentMorph.submorphs.length;
+                        var morphBefore = key < length ? parentMorph.submorphs[key + 1] : null;
                         if (parentMorph == this.world && value.length == 4 && Array.isArray(value[2].owner)) {
                             // need to reset owner to old value for proper removing in addMorph
                             obj[key].owner = value[2].owner.first();
                             var transform = obj[key].getTransform();
+                            morphBefore = this.world.firstHand();
                         }
-                        var length = parentMorph.submorphs.length;
-                        parentMorph.addMorph(obj[key],
-                                             key < length ? parentMorph.submorphs[key + 1] : null);
+                        parentMorph.addMorph(obj[key], morphBefore);
                         if (transform) obj[key].setTransform(transform);
                     }
                 } else if (key == "owner" && value.length == 2) {
