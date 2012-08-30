2a8797035328690d313fe4b9b6e4000686ba676f
Christopher Schuster <cs@livoris.de>
2012-08-30 18:18:19 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/client.js

--- client.js
+++ client.js
@@ -738,7 +738,6 @@ cop.create("SyncNewMorphs").refineObject(lively.morphic.World.current(), {
     addMorph: function(morph, optMorphBefore) {
         var result = cop.proceed(morph, optMorphBefore);
         SyncNewMorphs.wc.addObject(morph);
-        if (!isNaN(morph.id)) morph.setNewId();
         return result;
     },
     removeMorph: function(morph) {
