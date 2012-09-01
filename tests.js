efec2782d421f74f24c72a4a717674ec384781e1
Christopher Schuster <cs@livoris.de>
2012-09-01 01:44:47 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -450,7 +450,6 @@ lively.morphic.tests.MorphTests.subclass('users.cschuster.sync.tests.DiffTest',
         var snapshotA = this.serialize({X:x});
         x.a.push(y);
         var snapshotB = this.serialize({X:x});
-        debugger;
         this.assertPatch({"X/a/0": [{name:"y"}]}, snapshotA, snapshotB);
         x.a.push(z);
         var snapshotC = this.serialize({X:x});
@@ -458,7 +457,7 @@ lively.morphic.tests.MorphTests.subclass('users.cschuster.sync.tests.DiffTest',
         this.assertPatch({"X/a/0": [{name:"y"}], "X/a/1": [{name:"z"}]}, snapshotA, snapshotC);
         x.a[0] = z;
         var snapshotD = this.serialize({X:x});
-        var expected = {"X":{a:{1:{id:["X/a/0"]}}},"X/a/0":{name:["z"]},"X/a/1":[0,0]};
+        var expected = {"X":{a:{_t:"a",1:{id:["X/a/0"]}}},"X/a/0":{name:["z"]},"X/a/1":[0,0]};
         this.assertPatch(expected, snapshotC, snapshotD);
         x.a.removeAt(0);
         x.a[0] = y;
