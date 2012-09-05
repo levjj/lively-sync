d0fd0b7d32424c6715a4e368491c07d4e234cc95
Christopher Schuster <cs@livoris.de>
2012-09-05 22:45:05 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -1669,27 +1669,6 @@ users.cschuster.sync.tests.SyncTest.subclass('users.cschuster.sync.tests.SyncPri
         this.wcA.removeObject(box);
         this.assertSync(3);
     },
-    testList: function() {
-        var bounds = new lively.Rectangle(0, 0, 80, 60);
-        var list = new lively.morphic.List(bounds, []);
-        this.openInWorldA(list);
-        this.assertSync(2);
-        list.addItem({string: 'foo', value: 23, isListItem: true});
-        this.assertSync(3);
-        list.addItem({string: 'bar', value: 42, isListItem: true});
-        this.assertSync(4);
-        list.selectAt(0);
-        this.assertSync(5);
-        list.selectAt(1);
-        this.assertSync(6);
-        list.deselectAt(1);
-        this.assertSync(7);
-        list.removeItemOrValue(23);
-        this.assertSync(8);
-        list.updateList(["h", "e", "l", "l", "o"].asListItems());
-        this.assertSync(9);
-    }
-
 });
 
 users.cschuster.sync.tests.SyncTest.subclass('users.cschuster.sync.tests.WorkFlowTest',
