7ccc80d27d832757c777bd3e8e248c6ec6ef738c
Christopher Schuster <cs@livoris.de>
2012-09-05 22:45:10 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -1702,6 +1702,10 @@ users.cschuster.sync.tests.SyncTest.subclass('users.cschuster.sync.tests.WorkFlo
         game.setup();
         this.assertSync(3);
     },
+    newMethod: function() {
+        // enter comment here
+    }
+
 
 });
 
