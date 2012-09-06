0172c920300ed2e4880cd37857b4fd069ff393ba
Christopher Schuster <cs@livoris.de>
2012-09-06 07:35:20 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/tests.js

--- tests.js
+++ tests.js
@@ -994,7 +994,7 @@ lively.morphic.tests.MorphTests.subclass('users.cschuster.sync.tests.MorphPatchT
 },
 'specs', {
 
-    movePatch: {"X/_Position": {x: [5], y: [3]} },
+    movePatch: {"X": {_Position: ["\\$@lively.pt(5.0,3.0)"]}},
     resizePatch: {"X/shape/_Extent": {x: [13], y: [7]} },
     colorPatch: {"X/shape/_Fill": [{r: 0.5, g: 0.0, b: 1.0, a:1,
                                     __LivelyClassName__:"Color",
