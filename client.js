fd1b067765986fbbe3376dcde8a652d2de467ffb
Christopher Schuster <cs@livoris.de>
2012-09-25 18:59:41 +0000

    Autoversioning commit:  a non-deltaV client made a change to /users/cschuster/sync/client.js

--- client.js
+++ client.js
@@ -791,6 +791,7 @@ Object.subclass('users.cschuster.sync.WorkingCopy',
         "F": Color.web.rosybrown
     },
     addHand: function(hand) {
+        debugger;
         if (hand === lively.morphic.World.current().firstHand()) {
             if (!this.newHand) { hand.setNewId(); this.newHand = true; }
             hand.username = lively.morphic.World.current().getUserName();
